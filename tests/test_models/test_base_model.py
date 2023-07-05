443fb113941eab5f9bd92deb579805cb1c5fddda
Diego-Bonora <bonoradi@gmail.com>
2023-07-05 09:56:08 -0700

    first unitests for base moBaseModel class

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Unittest for BaseModel class
+"""
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def test_create_class(self):
+        b1 = BaseModel()
+        self.assertIsInstance(b1, BaseModel)
+        self.assertIsInstance(b1.id, str)
+        self.assertIsInstance(b1.created_at, datetime)
+
+    def test_save(self):
+        b1 = BaseModel()
+        old_datetime = b1.updated_at
+        b1.save()
+        new_datetime = b1.updated_at
+        self.assertNotEqual(old_datetime, new_datetime)
+
+    def test_dict(self):
+        b1 = BaseModel()
+        new_dict = b1.to_dict()
+        self.assertIsInstance(new_dict, dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
