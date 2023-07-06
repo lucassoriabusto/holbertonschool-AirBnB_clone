f2058504d99c24187723aed766efb6ca74057183
Diego-Bonora <bonoradi@gmail.com>
2023-07-06 13:56:03 -0700

    first tests for the file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Unittest for BaseModel class
+"""
+import unittest
+import os
+import json
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.storage = FileStorage()
+        self.path = "file.json"
+
+    def tearDown(self) -> None:
+        if os.path.exists(self.path):
+            os.remove(self.path)
+
+    def test_all(self):
+        self.assertAlmostEqual(self.storage.all(), {})
+
+    def test_create_new(self):
+        obj = BaseModel()
+        self.storage.new(obj)
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.assertAlmostEqual(key in self.storage.all(), True)
+
+    def test_save(self):
+        obj = BaseModel()
+        self.storage.save()
+        self.assertAlmostEqual(os.path.exists(self.path), True)
+        with open(self.path, mode="r") as file:
+            text = json.dumps(file.read())
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.assertAlmostEqual(key in text, True)
+
+    def test_reload(self):
+        obj = BaseModel()
+        self.storage.save()
+        self.storage.reload()
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.assertAlmostEqual(key in self.storage.all(), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
