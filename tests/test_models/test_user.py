a2963e565bbc645aa8c7af9e8ee0229cadbc6a06
Diego-Bonora <bonoradi@gmail.com>
2023-07-07 12:00:51 -0700

    new tests added

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,21 +16,25 @@ class TestUser(unittest.TestCase):
     def test_create_email(self):
         u1 = User()
         u1.email = "pepito89@hotmail.com"
+        self.assertAlmostEqual(u1.email, "pepito89@hotmail.com")
         self.assertIsInstance(u1.email, str)
 
-    def test_create_email(self):
+    def test_create_password(self):
         u1 = User()
         u1.password = "password"
+        self.assertAlmostEqual(u1.password, "password")
         self.assertIsInstance(u1.password, str)
 
-    def test_create_email(self):
+    def test_create_first_name(self):
         u1 = User()
         u1.first_name = "Pepe"
+        self.assertAlmostEqual(u1.first_name, "Pepe")
         self.assertIsInstance(u1.first_name, str)
 
-    def test_create_email(self):
+    def test_create_last_name(self):
         u1 = User()
         u1.last_name = "Noches"
+        self.assertAlmostEqual(u1.last_name, "Noches")
         self.assertIsInstance(u1.last_name, str)
 
 
