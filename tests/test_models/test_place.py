9303a16b9ecb35b16a7bab037dcbda9897dd9127
Diego-Bonora <bonoradi@gmail.com>
2023-07-08 15:37:36 -0300

    added tests for the place class

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Unittest for Place class
+"""
+import unittest
+from models.place import Place
+
+
+class TestPlace(unittest.TestCase):
+
+    def test_create_class(self):
+        p1 = Place()
+        self.assertIsInstance(p1, Place)
+        self.assertTrue(hasattr(p1, "city_id"))
+        self.assertTrue(hasattr(p1, "user_id"))
+        self.assertTrue(hasattr(p1, "name"))
+        self.assertTrue(hasattr(p1, "description"))
+        self.assertTrue(hasattr(p1, "number_rooms"))
+        self.assertTrue(hasattr(p1, "number_bathrooms"))
+        self.assertTrue(hasattr(p1, "max_guest"))
+        self.assertTrue(hasattr(p1, "price_by_night"))
+        self.assertTrue(hasattr(p1, "latitude"))
+        self.assertTrue(hasattr(p1, "longitude"))
+        self.assertTrue(hasattr(p1, "amenity_ids"))
+        
+    def test_empty_attributes(self):
+        p1 = Place()
+        self.assertAlmostEqual(p1.city_id, "")
+        self.assertAlmostEqual(p1.user_id, "")
+        self.assertAlmostEqual(p1.name, "")
+        self.assertAlmostEqual(p1.description, "")
+        self.assertAlmostEqual(p1.number_rooms, 0)
+        self.assertAlmostEqual(p1.number_bathrooms, 0)
+        self.assertAlmostEqual(p1.max_guest, 0)
+        self.assertAlmostEqual(p1.price_by_night, 0)
+        self.assertAlmostEqual(p1.latitude, 0.0)
+        self.assertAlmostEqual(p1.longitude, 0.0)
+        self.assertAlmostEqual(p1.amenity_ids, [])
+
+
+if __name__ == "__main__":
+    unittest.main()
